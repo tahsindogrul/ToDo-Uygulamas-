251fd5926fb952c59dd344f57e4b16ff14895dd3
tahsindogrul <tahsindogrul@icloud.com>
2024-07-04 21:24:41 +0300

    app user DTO oluşturuldu

---
+++ ToDo.Models/DTOs/AppUserDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.Models.DTOs
+{
+    public class AppUserDto:AppUser
+    {
+        public int TotalTodo { get; set; }
+    }
+}
