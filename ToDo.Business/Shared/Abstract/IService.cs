b73e38ad5c37fbd5032460ec0a9b7cee40f2693b
tahsindogrul <tahsindogrul@icloud.com>
2024-07-04 21:23:41 +0300

    temel servisler oluşturuldu.

---
+++ ToDo.Business/Shared/Abstract/IService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using ToDo.Models;
+
+namespace ToDo.Business.Shared.Abstract
+{
+    public interface IService<T> where T : BaseModel ,new()
+    {
+        T GetById(int id);
+        T Add(T entity);
+        ICollection<T> AddRange(ICollection<T> entities);
+
+        T Update(T entity);
+        bool Delete(int id);
+
+        IQueryable<T> GetAll();
+
+        IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
+
+        T GetFirstOrDefault(Expression<Func<T, bool>> expression);
+
+      
+    }
+}
---
+++ ToDo.Business/Shared/Concrete/Service.cs
@@ -0,0 +1,66 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using ToDo.Business.Shared.Abstract;
+using ToDo.Data.Repository.Shared.Absract;
+using ToDo.Models;
+
+namespace ToDo.Business.Shared.Concrete
+{
+    public class Service<T> : IService<T> where T : BaseModel, new()
+    {
+        private readonly IRepository<T> _repo;
+
+        public Service(IRepository<T> repo)
+        {
+            _repo = repo;
+        }
+
+        public T Add(T entity)
+        {
+            return _repo.Add(entity);
+           
+            
+        }
+
+        public ICollection<T> AddRange(ICollection<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            _repo.Delete(id);
+            return true;
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _repo.GetAll();
+        }
+
+        public IQueryable<T> GetAll(Expression<Func<T, bool>> expression)
+        {
+            return _repo.GetAll(expression);
+        }
+
+        public T GetById(int id)
+        {
+           return _repo.GetById(id);
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> expression)
+        {
+            return _repo.GetFirstOrDefault(expression);
+        }
+
+        public T Update(T entity)
+        {
+            return _repo.Update(entity);
+        }
+    }
+}
